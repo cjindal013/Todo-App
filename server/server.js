54f2e6180f1a3083386d0817d237958d748b5e64
Chirag Jindal <chiragjindal2169@gmail.com>
2019-09-01 13:18:28 +0530

    Update option has been added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express= require('express');
-var bodyParser= require('body-parser');
-var {ObjectID}= require('mongodb');
+const _= require('lodash');
+const express= require('express');
+const bodyParser= require('body-parser');
+const {ObjectID}= require('mongodb');
 
 var app= express();
 
@@ -58,6 +59,26 @@ app.delete('/todos/:id',(req,res)=>{
 	});
 });
 
+app.patch('/todos/:id',(req,res)=>{
+	var id=req.params.id;
+	var body=_.pick(req.body,['text','completed']);
+	if(!ObjectID.isValid(id))
+		return res.status(404).send('Id not valid');
+	if(_.isBoolean(body.completed) && body.completed)
+		body.completedAt= new Date().getTime();
+	else
+	{
+		body.completed=false;
+		body.completedAt=null;
+	}
+	Todo.findByIdAndUpdate(id,{$set: body},{new: true}).then((todo)=>{
+		if(!todo)
+			return res.status(404).send('Empty todo');
+		return res.send({todo});
+	}).catch((e)=>{
+		return res.status(400).send('Not exist');
+	});
+});
 
 app.listen(port,()=>{
 	console.log(`Server start at ${port}`);
