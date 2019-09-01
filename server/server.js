47c4c900b3c7b1e23a5299d0536fbc2916db8e0d
Chirag Jindal <chiragjindal2169@gmail.com>
2019-09-01 12:54:34 +0530

    Add delete note

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
+const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 
 app.post('/todos',(req,res)=>{
@@ -16,34 +18,47 @@ app.post('/todos',(req,res)=>{
 	});
 
 	todo.save().then((doc)=>{
-		res.send(doc);
+		return res.send(doc);
 	},(e)=>{
-		res.status(400).send(e);
+		return res.status(400).send(e);
 	});
 });
 
 app.get('/todos',(req,res)=>{
 	Todo.find().then((todos)=>{
-		res.send({todos});
+		return res.send({todos});
 	},(e)=>{
-		res.status(400).send(e);
+		return res.status(400).send(e);
 	});
 });
 
 app.get('/todos/:id',(req,res)=>{
 	var id=req.params.id;
 	if(!ObjectID.isValid(id))
-		res.status(404).send('Id not valid');
+		return res.status(404).send('Id not valid');
 	Todo.findById(id).then((todo)=>{
 		if(!todo)
-			res.status(404).send('Empty todo');
-		res.send({todo});
+			return res.status(404).send('Empty todo');
+		return res.send({todo});
+	}).catch((e)=>{
+		return res.status(400).send('Not exist');
+	});
+});
+
+app.delete('/todos/:id',(req,res)=>{
+	var id=req.params.id;
+	if(!ObjectID.isValid(id))
+		return res.status(404).send('Id not valid');
+	Todo.findByIdAndRemove(id).then((todo)=>{
+		if(!todo)
+			return res.status(404).send('Empty todo');
+		return res.send({todo});
 	}).catch((e)=>{
 		return res.status(400).send('Not exist');
 	});
 });
 
 
-app.listen(3000,()=>{
-	console.log('Started');
+app.listen(port,()=>{
+	console.log(`Server start at ${port}`);
 });
